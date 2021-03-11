8ef9523d4cee9b572941eb89a03237287fe7b9c9
hyeon ji <inthefield1027@gmail.com>
2021-03-11 14:21:15 +0900

    add card and edit form component

--- src/components/maker/maker.js
+++ src/components/maker/maker.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,43 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: 1,
+            name: 'Hyeon Ji',
+            company: 'Ola',
+            theme: 'dark',
+            title: 'software Engineer',
+            email: 'inthefield1027@gmail.com',
+            message: 'go for it',
+            fileName: 'hj',
+            fileURL: null,
+        },
+
+        {
+            id: 2,
+            name: 'DAN BI',
+            company: 'ICIS',
+            theme: 'light',
+            title: 'Manager',
+            email: 'chl12005@gmail.com',
+            message: 'I can do it',
+            fileName: 'hj',
+            fileURL: 'hj.png',
+        },
+        {
+            id: 3,
+            name: 'Min Su',
+            company: 'Ola',
+            theme: 'colorful',
+            title: 'Engineer',
+            email: '12345@gmail.com',
+            message: 'code your dream',
+            fileName: 'hj',
+            fileURL: null,
+        },
+    ]);
+
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -23,8 +60,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} className={styles.header} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer className={styles.footer} />
         </section>
